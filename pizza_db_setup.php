3c14fb730e831a37f565cf9ad5cfd43d18a2cf45
Mark Goldberg <markiragoldberg@outlook.com>
2017-04-14 16:28:22 -0500

    they are closing the lab I am working in

--- pizza_db_setup.php
+++ pizza_db_setup.php
@@ -16,34 +16,22 @@
         die("Unable to connect to the database");
     }
     
-    /*
-     * Drop old tables if they exist
-     */
-     
-     sql_query($con, "DROP TABLE pizza_orders;");
-     sql_query($con, "DROP TABLE pizza_customers;");
-     sql_query($con, "DROP TABLE pizza_items;");
-    
-    /*
-     * Recreate tables to latest standards
-     */
-    $sql = "CREATE TABLE pizza_orders (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            customer_id INT(6) UNSIGNED
-            )";
-    sql_query($con, $sql);
-    
+    #Drop old tables if they exist
+    sql_query($con, "DROP TABLE pizza_customers;");
+    sql_query($con, "DROP TABLE pizza_items;");
+
+    # Recreate tables to latest standards
     $sql = "CREATE TABLE pizza_customers (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(50),
-            phone int(11),
-            credit_card int (19)
+            phone VARCHAR(11),
+            credit_card VARCHAR(19)
             )";
     sql_query($con, $sql);
     
     $sql = "CREATE TABLE pizza_items (
-            id INT(6) UNSIGNED,
-            order_id INT(6) UNSIGNED,
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            customer_id INT(6) UNSIGNED,
             item_type TINYINT,
             item_descriptors SET('a', 'b', 'c', 'd')
             )";
