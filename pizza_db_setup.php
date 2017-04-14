0f4826ccc1b062f568ab7b7cd53da3921ba747ac
Mark Goldberg <markiragoldberg@outlook.com>
2017-04-14 13:03:14 -0500

    Revised db structure

--- pizza_db_setup.php
+++ pizza_db_setup.php
@@ -1,4 +1,11 @@
 <?php
+    function sql_query($con, $sql) {
+        if(!mysqli_query($con, $sql)) {
+            echo "Error with query '" . $sql . "'.<br/>";
+        }
+    }
+
+
     $servername = "dbserv.cs.siu.edu";
     $username = "mgoldberg";
     $password = "Eu7BugDf";
@@ -10,112 +17,37 @@
     }
     
     /*
-     * Tables
-     * 
-     * orders
-     * customers
-     * items
-     * pizzas, pizzatoppingsets, pizzatoppings, crusts, sauces, sizes
-     * drinks, drinktypes
+     * Drop old tables if they exist
      */
+     
+     sql_query($con, "DROP TABLE pizza_orders;");
+     sql_query($con, "DROP TABLE pizza_customers;");
+     sql_query($con, "DROP TABLE pizza_items;");
     
-    $sql = "CREATE TABLE orders (
+    /*
+     * Recreate tables to latest standards
+     */
+    $sql = "CREATE TABLE pizza_orders (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             customer_id INT(6) UNSIGNED
             )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating orders table.<br/>";
-    }
+    sql_query($con, $sql);
     
-    $sql = "CREATE TABLE customers (
+    $sql = "CREATE TABLE pizza_customers (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(50),
             phone int(11),
             credit_card int (19)
             )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating customers table.<br/>";
-    }
+    sql_query($con, $sql);
     
-    $sql = "CREATE TABLE items (
+    $sql = "CREATE TABLE pizza_items (
             id INT(6) UNSIGNED,
+            order_id INT(6) UNSIGNED,
             item_type TINYINT,
-            order_id INT(6) UNSIGNED
+            item_descriptors SET('a', 'b', 'c', 'd')
             )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating items table.<br/>";
-    }
-    
-    
-    $sql = "CREATE TABLE pizzas (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            pizzatopping_id INT(6) UNSIGNED,
-            item_count SMALLINT,
-            pizzasize_type TINYINT UNSIGNED,
-            crust_type TINYINT UNSIGNED,
-            sauce_type TINYINT UNSIGNED
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzas table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE pizzatoppings (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            topping_type TINYINT UNSIGNED
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzatoppings table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE pizzatopping_types (
-            id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name varchar(20)
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzatopping_types table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE pizzasize_types (
-            id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name varchar(20),
-            price INT(4) UNSIGNED
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzasize_types table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE pizzasauce_types (
-            id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name varchar(20)
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzasauce_types table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE pizzacrust_types (
-            id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name varchar(20),
-            price INT(4) UNSIGNED
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating pizzacrust_types table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE drinks (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            drink_type TINYINT UNSIGNED
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating drinks table.<br/>";
-    }
-    
-    $sql = "CREATE TABLE drink_types (
-            id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name varchar(50)
-            )";
-    if(!mysqli_query($con, $sql)) {
-        echo "Error creating drink_types table.<br/>";
-    }
+    sql_query($con, $sql);
     
     mysqli_close($con);
 ?>
