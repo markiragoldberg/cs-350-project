fd5c65b42d0f232bfa9b37cbebaf730ab5a627ff
coltenleidner <coltenleidner@siu.edu>
2017-04-15 15:03:20 -0500

    Update OrderPizza.php

--- OrderPizza.php
+++ OrderPizza.php
@@ -5,7 +5,7 @@
         }
     }
     //insert the name used
-    $pizza = $_POST[" "];
+    $item = $_POST[" "];
     $name = $_POST[" "];
     $phone = $_POST[" "];
     $card = $_POST[" "];
@@ -26,18 +26,48 @@
     
     $sql = "SELECT id from pizza_customers where phone='$phone'";
     $customerid = (sql_query($con, $sql));
-    
+
+     switch ($item[0]{
+         
+         case "pizza":
 
     //section for inserting toppings into table
-    $toppings = split (", ", $pizza);
+    $toppings = split (", ", $item);
     $size = count($toppings);
     $set;
-    for ($i=0; i < $size; i++){
+    for ($i=1; i < $size; i++){
     $sql = "SELECT id from pizza_toppings where Topping = '$toppings[$i]'";
     $nextTopping = (sql_query($con, $sql));
     $set->add($nextTopping);
     }
-    
+    $itemtype = 0;
+         
+         break;
+         case   "calzone":
+         
+         $itemtype = 1;
+         break;
+         
+         case   "salad":
+         
+         $itemtype = 2;
+         break;
+         
+         case   "breadsticks":
+         
+         $itemtype = 3;
+         break;
+         
+         case   "drink":
+         
+         $itemtype = 4;
+         break;
+         
+         
+         
+         
+         
+         
     $sql = "INSERT INTO pizza_items (customer_id, item_type, item_descriptors) VALUES ('$customerid', '$itemtype', '$set')";
     if($result=(sql_query($con, $sql))){
         
