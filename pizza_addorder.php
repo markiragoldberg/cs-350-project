97b351bad1e138622bcd3ba9b5b3981d81074e0b
Mark Goldberg <markiragoldberg@outlook.com>
2017-04-17 11:11:02 -0500

    Replace use of broken deprecated function

--- pizza_addorder.php
+++ pizza_addorder.php
@@ -28,7 +28,7 @@
     sql_query($con, "INSERT INTO pizza_customers (name, phone, credit_card) VALUES ('$name', '$phone', '$credit_card')");
     
     # ... get autoincremented ID of customer we just inserted!
-    $customer_id = mysql_insert_id();
+    $customer_id = mysqli_insert_id($con);
     
     sql_query($con,"INSERT INTO pizza_items (customer_id, item_type, item_descriptors) VALUES ('$customer_id', '$item_type', '$item_descriptors')");
     
