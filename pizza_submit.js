eef389c7241a99e17e117fc9804a2b26b2bd994e
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-04-23 22:17:41 -0500

    Revert Donald's image based menu
    The image inputs completely break the menu; they function as submit buttons. Instead they should be labels for hidden checkboxes.

--- pizza_submit.js
+++ pizza_submit.js
@@ -36,11 +36,3 @@ document.getElementById("pizza_form").onsubmit = function() {
     xhr.send("pizza=" + submit_string);
     */
 };
-
-function click_color(id){
-	var target = document.getElementById(id);
-	if (target.style.border == "3px solid black")
-		target.style.border = "3px solid red";
-	else
-		target.style.border = "3px solid black";
-}
