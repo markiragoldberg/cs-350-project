fa2cfcf418a0e6ea9dad123c55cc395a41d61b6e
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-03 12:13:53 -0500

    Add function to update displayed price of pizza

--- pizza_submit.js
+++ pizza_submit.js
@@ -23,6 +23,11 @@ document.getElementById("pizza_form").onsubmit = function() {
         return;
     }
     console.log(submit_string);
+    
+    // debug pizza price before using it in display
+    let price = calculate_pizza_price();
+    console.log("pizza price: " + price);
+    
     document.getElementById("item").value = submit_string;
     /*
     var xhr = new XMLHttpRequest();
@@ -37,9 +42,48 @@ document.getElementById("pizza_form").onsubmit = function() {
     */
 };
 
+function calculate_pizza_price() {
+    let form = document.getElementById("pizza_form");
+    let elements = form.getElementsByTagName("input");
+    var price = 0.0;
+    for(var i = 0; i < elements.length; ++i) {
+        if(elements[i].checked) {
+            switch(elements[i].getAttribute("name"))
+            {
+                case "size":
+                    switch(elements[i].getAttribute("id"))
+                    {
+                        case "small":
+                            price += 7.00;
+                            break;
+                        case "medium":
+                            price += 10.50;
+                            break;
+                        case "large":
+                        default:
+                            price += 14.00;
+                            break;
+                    }
+                    break;
+                case "sauce":
+                    // sauce is free
+                    break;
+                default:
+                    // all toppings are $0.75
+                    price += 0.75;
+            }
+        }
+    }
+    return price;
+}
+
 function click_color(target){
 	if (target.style.border == "5px solid green")
 		target.style.border = "5px solid white";
 	else
 		target.style.border = "5px solid green";
 }
+
+window.onload = function() {
+    
+};
