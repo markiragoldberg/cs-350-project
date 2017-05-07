44aae789db91caa4634c918dd429df24aec691af
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-07 15:55:12 -0500

    Redirect after confirming an order
    Instead of displaying the debug info, redirect to the pizza menu and log you in for order tracking.

--- OrderPizza.php
+++ OrderPizza.php
@@ -125,4 +125,9 @@
     
     $getCustomerId->close();
     mysqli_close($con);
+    
+    // Log you in for the order tracker
+    // Redirect you to the pizza menu
+    $_SESSION['phone'] = $phone;
+    header('Location: pizza_menu.php');
 ?>
