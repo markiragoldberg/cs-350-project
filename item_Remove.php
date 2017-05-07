b0c7e6c5d87ebc5763495296bce8a35085936c78
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2017-05-07 02:08:34 -0500

    use PDO

--- item_Remove.php
+++ item_Remove.php
@@ -1,13 +1,6 @@
 <?php
     $itemid = $_GET["item"];
-    function sql_query($con, $sql) {
-        $result = mysqli_query($con, $sql);
-        if(!$result) {
-            return "Error with query '" . $sql . "'.<br/>";
-        } else {
-            return $result;
-        }
-    }
+    
     $servername = "dbserv.cs.siu.edu";
     $username = "mgoldberg";
     $password = "Eu7BugDf";
@@ -17,9 +10,15 @@
     {
         die("Unable to connect to the database");
     }
-    $sql = "DELETE FROM pizza_items WHERE id = '$itemid'";
-    $result = (sql_query($con, $sql));
+    
+    $sql = "DELETE FROM pizza_items WHERE id = ?";
+    $deleteRow = $con->prepare($sql);
+    $deleteRow->bind_param("i", $itemid);
+    $deleteRow->execute();
+    $result = mysqli_stmt_get_result($deleteRow);
     print $result;
+    
+    $deleteRow->close();
     mysqli_close($con);
     
-    ?>
+?>
